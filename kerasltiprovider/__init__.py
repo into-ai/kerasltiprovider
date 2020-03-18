48bcf10e77a3bb60d476131b3ec7ff969ae8a555
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2020-03-18 11:23:48 +0100

    Bump version: 0.1.0 → 0.2.0

--- kerasltiprovider/__init__.py
+++ kerasltiprovider/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """into-ai"""
 __email__ = "introintoai@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from kerasltiprovider.kerasltiprovider import KerasLTIProvider as _provider
 
